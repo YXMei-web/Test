0f4bb4df719a36757672eaf769716b5f74b109ce
Yuxiao <m1228yxo@gmail.com>
2024-04-26 22:56:25 +0200

    first commit

---
+++ Github Test/main.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Add.h"
+#include <stdio.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int c;
+	c= Add(2,4);
+	printf("%d\n",c);
+	return 0;
+}
